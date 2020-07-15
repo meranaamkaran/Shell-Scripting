3f8f8c573e1869cd6543f65e98cf34678c04b31f
meranaamkaran <kotyadasrikaran@gmail.com>
2020-07-15 15:35:08 +0530

    ADDING THE SAMPLE FILES---1 more COMMENTS

---
+++ 01-she-bang.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+
+# SH  - BOURNE SHELL -> HPUX/ AT&T -> Simple in Syntaxes
+# KSH - KORN SHELL   -> IBM        -> Login Scripts
+# CSH - C SHELL      -> SUNOS      -> Index Variables
+
+
+# BASH  -> Born again Shell  / Bourne again Shell
+# BASH  -> SH + KSH + CSH , Default in Linux
+## WE CAN GIVE .SH OR .BASH EXTENSION ##
+
+# ZSH   -> DESKTOP SHELL EX. MACMACHINE
+
+#  (#!)  IS CALLED
+# SHE-BANG ->> #!
+       # DENOTES WHICH SCRIPT IT IS
+       # ENSURES REMAINING CODE EXCECUTES USING THAT INTERPRETOR
+
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # shell-scripting
+
+She-Bang
+Comments
+Print
+Variables
+Input
+Redirectors, Exit states, Quotes ,
+Functions
+Conditions
+Loops 

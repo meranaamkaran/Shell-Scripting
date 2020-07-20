6c93874c36dfd2b0c4adbe3b95beed2fe9989c33
meranaamkaran <kotyadasrikaran@gmail.com>
2020-07-20 13:58:47 +0530

    ADDING THE SAMPLE FILES---1 more

--- 03-variables.sh
+++ 03-variables.sh
@@ -11,8 +11,18 @@ b=xyz
 c=abcd1234
 #boolean
 d=true | false
+#float
+e=7.7777
 
 
 ##Acess a variable = $var_name | ${var_name}
 
 echo value of a=$a
+
+NAME= KARAN
+SEVICES= PHOTOGRAPHY
+
+echo -e "WELCOME TO $NAME PHOTOGRAPHY"
+echo -e "WE PROVIDE $SEVICES FOR ALL TYPES OF EVENTS"
+
+

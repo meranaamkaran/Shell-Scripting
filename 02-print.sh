85b14b5bad66390bd971418deb4c62599b3d6d26
meranaamkaran <kotyadasrikaran@gmail.com>
2020-07-20 12:34:02 +0530

    ADDING THE SAMPLE FILES---1 more

--- 02-print.sh
+++ 02-print.sh
@@ -8,3 +8,15 @@ EXAMPLES!!
 NOTE
 
 echo -e "KARAN \nHello World"
+
+#COLOUR CODES
+#
+#RED                  31
+#GREEN                32
+#YELLOW               33
+#BLUE                 34
+#MAGENTA              35
+#CYAN                 36
+#RESET                0
+
+echo -e "\e[31mHELLO WORLD IN RED\e[0m \n\e[32mHELLO WORLD IN GREEN\e[0m"

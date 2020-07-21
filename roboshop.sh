978892ec48582b3676c574010d20df8dd11a1b1c
meranaamkaran <kotyadasrikaran@gmail.com>
2020-07-21 14:22:53 +0530

    ADDING THE SAMPLE FILES---1 more

--- roboshop.sh
+++ roboshop.sh
@@ -9,6 +9,19 @@ R="\e[1;31m"
 N="\e[0m"
 
 
-echo -e "[${B}INF${N}] MESSAGE"
-echo -e "[${Y}WAR${N}] WARNING"
-echo -e "[${R}ERR${N}] ERROR"
+#echo -e "[${B}INF${N}] MESSAGE"
+#echo -e "[${Y}WAR${N}] WARNING"
+#echo -e "[${R}ERR${N}] ERROR"
+
+
+echo -e "[${B}INF${N}] MONGODB"
+echo -e "[${B}INF${N}] MYSQL"
+echo -e "[${B}INF${N}] REDIS"
+echo -e "[${B}INF${N}] RABBITMQ"
+echo -e "[${B}INF${N}] FRONTEND"
+echo -e "[${B}INF${N}] CART"
+echo -e "[${B}INF${N}] CATALOGUE"
+echo -e "[${B}INF${N}] USER"
+echo -e "[${B}INF${N}] PAYMENT"
+echo -e "[${B}INF${N}] SHIPPING"
+echo -e "[${B}INF${N}] RATTINGS"

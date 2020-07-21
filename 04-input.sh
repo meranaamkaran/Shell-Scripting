db3d7e6fd8cf97e3c523115fd679581b36ba2631
meranaamkaran <kotyadasrikaran@gmail.com>
2020-07-21 11:38:22 +0530

    ADDING THE SAMPLE FILES---1 more

--- 04-input.sh
+++ 04-input.sh
@@ -2,9 +2,23 @@
 
 ##inputs are of two types##
 #input during excecution
+#input before excecution
 
 ##input during excecution
 
-read -p "enter yor name: " name
+#read -p "enter yor name: " name
+#
+#echo  "Name = $name"
 
-echo  "Name = $name"
+
+#Read command is not widely used in automation. Reason is ,It just prompts.
+#Meaning is cannot proceed further until we provide input
+
+#input before excecution
+
+echo "Script Name = $0"
+echo "First Argument = $1"
+echo "Secound Argument = $2"
+
+## $1-$n are the order of arguments
+

fde09372b07181c16a7c393a8c4e099a034cc6ae
meranaamkaran <kotyadasrikaran@gmail.com>
2020-07-21 14:06:47 +0530

    ADDING THE SAMPLE FILES---1 more

---
+++ roboshop.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+#Colour Codes
+
+B="\e[1;34m"
+Y="\e[1;33m"
+R="\e[1;31m"
+N="\e[0m"
+
+
+echo -e "[${B}INF${N}] MESSAGE"
+echo -e "[${B}WAR${N}] WARNING"
+echo -e "[${B}ERR${N}] ERROR"

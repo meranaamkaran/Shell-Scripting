1664c141578d728753091fc8e0d67bd2462377e8
meranaamkaran <kotyadasrikaran@gmail.com>
2020-07-23 14:06:30 +0530

    pushing

---
+++ 07-conditions.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+##CONDITIONS ARE TWO TYPES##
+#1>CASE
+#2>IF
+
+
+#CASE COMMAND
+
+<SYNTAX
+case variable in
+    pattern1)
+        commands
+        ;;
+    pattern2)
+        commands
+        ;;
+esac
+SYNTAX
+
+
+
+case $fruit_name in
+       apple)
+         echo fruit name is apple
+         ;;
+       bannana)
+         echo fruit name is banana
+         ;;
+esac

23a9d5879347644505690db2dabb9dd94cc73b0f
meranaamkaran <kotyadasrikaran@gmail.com>
2020-07-23 12:36:51 +0530

    ADDING THE SAMPLE FILES---1 more

---
+++ function.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+#Variable:if we assign a name to set of data is called as variable.
+#Function:if we assign a name to set of coomands is called as function.
+
+<<format
+TO DECLARE Function
+
+function sample()
+{
+commands
+commands
+}
+
+||or||
+
+sample()
+{
+commands
+commands
+}
+how to call a function
+sample
+format
+
+
+f()
+{
+  echo iam a function
+  a=100
+  echo a = ${a}
+  echo i am a function, i can call variables from main program, b = {b}
+}
+b=200
+f
+
+
+
